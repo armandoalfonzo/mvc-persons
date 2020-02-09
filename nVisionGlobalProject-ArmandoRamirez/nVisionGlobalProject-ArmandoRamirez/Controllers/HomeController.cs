2c8d362681350438995df94b6d01a1b1321f7cb2
armandoalfonzo <armandoalfonzo98@gmail.com>
2020-02-09 13:57:16 -0600

    Fisrt Commit

---
+++ nVisionGlobalProject-ArmandoRamirez/nVisionGlobalProject-ArmandoRamirez/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using nVisionGlobalProject_ArmandoRamirez.Models;
+
+namespace nVisionGlobalProject_ArmandoRamirez.Controllers
+{
+    public class HomeController : Controller
+    {
+        private string JsonRoot = $@"{System.IO.Directory.GetCurrentDirectory()}\wwwroot\Json";
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(PersonViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                List<PersonViewModel> people = new List<PersonViewModel>();
+
+                if (System.IO.File.Exists(JsonRoot))
+                {
+                    people = JsonConvert.DeserializeObject<List<PersonViewModel>>(System.IO.File.ReadAllText(JsonRoot));
+                }
+                people.Add(model);
+                System.IO.File.WriteAllText(JsonRoot, JsonConvert.SerializeObject(people));                
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public string GetJson()
+        {
+            if (System.IO.File.Exists(JsonRoot))
+            {
+                return System.IO.File.ReadAllText(JsonRoot);
+            }
+            return "";
+        }
+    }
+}
---
+++ nVisionGlobalProject-ArmandoRamirez/nVisionGlobalProject-ArmandoRamirez/Models/PersonViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace nVisionGlobalProject_ArmandoRamirez.Models
+{
+    public class PersonViewModel
+    {
+        [Required(ErrorMessage = "Please enter your firstname")]
+        public string Firstname { get; set; }
+
+        [Required(ErrorMessage = "Please enter your lastname")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Please enter a valid telephone")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Not a valid phone number")]
+        public string Telephone { get; set; }
+
+        [Required(ErrorMessage = "Please enter your gender")]
+        public string Gender { get; set; }
+
+        [Required(ErrorMessage = "Please enter your ocupation")]
+        public string Ocupation { get; set; }
+    }
+}
